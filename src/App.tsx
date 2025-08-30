c78832c8f5944319e16d91a603f7f981b177ec5a
Xmeng-Li <105245216+Xmeng-Li@users.noreply.github.com>
2025-08-29 17:42:55 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// All Webpage
+import Home from "./components/Home";
+import OfficeHours from "./components/officeHours/OfficeHours";  
+import Recording from "./components/officeHours/Recording";
+import OurTeam from "./components/OurTeam";
+import Webinar from "./components/webinar/Webinar";
+import AllVideo from "./components/webinar/Video";
+import Interview from "./components/programs/Interview";
+import Intern from "./components/programs/Intern";
+import Project from "./components/programs/Project";
+import FocusGroup from "./components/programs/FocusGroup";
+import Chronicle from "./components/chronicles/Chronicles";
+import Class from "./components/programs/Class";
+
+// Chronicle Contents
+import EveryoneIsDifferent from "./components/chronicles/EveryoneIsDifferent";
+import GoAheadAndAsk from "./components/chronicles/GoAheadAndAsk";
+import FailEarly from "./components/chronicles/FailEarly";
+import RemindYourself from "./components/chronicles/RemindYourself";
+import  StrongestPerson from "./components/chronicles/BecomeStrongestPerson";
+import CommunicationIsKey from "./components/chronicles/CommunicationIsKey";
+import CertificationHelps from "./components/chronicles/CertificationHelps";
+import BiggestHappiness from "./components/chronicles/BiggestHappiness";
+import DoSomethingYouLike from "./components/chronicles/DoSomethingYouLike";
+import YouCanDoThis from "./components/chronicles/YouCanDoThis";
+import PsychIsCalling from "./components/chronicles/PsychIsCalling";
+import InternshipsAreOpp from "./components/chronicles/InternshipsAreOpp";
+import ExploreRealWorld from "./components/chronicles/ExploreRealWorld";
+import GoodTech from "./components/chronicles/GoodTech";
+import ConstExposure from "./components/chronicles/ConstExposure";
+import StemNotNerds from "./components/chronicles/StemNotNerds";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/office-hours" element={<OfficeHours />} />
+          <Route path="/recording" element={<Recording />} />
+
+          <Route path="/our-team" element={<OurTeam />} />
+          <Route path="/webinar" element={<Webinar />} />
+          <Route path="/allVideo" element={<AllVideo />} /> 
+
+          <Route path="/interview" element={<Interview />} />
+          <Route path="/intern" element={<Intern />} />
+          <Route path="/focus-group" element={<FocusGroup />} />
+          <Route path="/beautiful-land-initiative" element={<Project />} />
+          <Route path="/chronicle" element={<Chronicle />} />
+          <Route path="/cce-class" element={<Class />} />
+
+          <Route path="/everyone-is-different" element={<EveryoneIsDifferent />}/>
+          <Route path="/go-ahead-and-ask" element={<GoAheadAndAsk />}/>
+          <Route path="/fail-early" element={<FailEarly />}/>
+          <Route path="/remind-yourself" element={<RemindYourself />}/>
+          <Route path="/strongest-person" element={<StrongestPerson />}/>
+          <Route path="/communication-is-key" element={<CommunicationIsKey />}/>
+          <Route path="/certification-helps" element={<CertificationHelps />}/>
+          <Route path="/biggest-happiness" element={<BiggestHappiness />}/>
+          <Route path="/do-something-you-like" element={<DoSomethingYouLike />}/>
+          <Route path="/you-can-do-this" element={<YouCanDoThis />}/>
+          <Route path="/psychology" element={<PsychIsCalling />}/>
+          <Route path="/internships-are-opp" element={<InternshipsAreOpp />}/>
+          <Route path="/explore-real-world" element={<ExploreRealWorld />}/>
+          <Route path="/good-tech" element={<GoodTech />}/>
+          <Route path="/const-exposure" element={<ConstExposure />}/>
+          <Route path="/stem-not-nerds" element={<StemNotNerds />}/>
+        </Routes>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,46 @@
+
+import React, { Suspense } from 'react';
+import { createRoot } from 'react-dom/client';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+
+i18n
+  .use(initReactI18next) 
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'zh', 'zh-CN'],
+    fallbackLng: "en",
+    detection: {
+      order: ['htmlTag', 'cookie', 'querystring', 'localStorage', 'sessionStorage', 'navigator', 'path', 'subdomain'],
+      caches: ['cookie']
+    },
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json'
+    },
+    ns: ['header', 'officehour', 'home','team', 'webinar', 'programs','class', 'chronicles', 'articles', 'footer'],
+    defaultNS: 'header'
+  });
+
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h2>Loading...</h2>
+  </div>
+);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement); // Use createRoot instead of render
+  root.render(
+    <Suspense fallback={loadingMarkup}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Suspense>
+  );
+}
